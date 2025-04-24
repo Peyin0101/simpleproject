db1eafcb2a34a9134c8e43a98607f31b509c6106
Randi Fernandez-Moran <randi.fernandez-moran@hotmail.com>
2025-04-24 12:23:35 +0200

    Add MyComponent.vue with initial template and data structure

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>{{ boodschapBinnenComponent }}</h3>
+</template>
+<script>
+export default {
+  name: "MyComponent",
+  data: function () {
+    return {
+      boodschapBinnenComponent: "Dit is een tekst van in de component.",
+    };
+  },
+};
+</script>
